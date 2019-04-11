11f1d00850152a4c4c3fd781ae36b6dbb774157e
Ryan Tate <rtate@willistonfinancial.com>
2019-04-10 18:04:27 -0700

    github pages build

---
+++ precache-manifest.df504e36f0a7f596dbb5cd35ffcc8681.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "ef713c65db1087cfb77b",
+    "url": "/tic-tac-toe/static/js/main.af4b1f0a.chunk.js"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/tic-tac-toe/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "ef713c65db1087cfb77b",
+    "url": "/tic-tac-toe/static/css/main.1e92669a.chunk.css"
+  },
+  {
+    "revision": "fe9d02cbdb8a9e66b55a95086ee1c252",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
